c3f552aeec9d251951366db6c5100bcebe2ffc0c
Benjamin Portelli <bportelli@gmail.com>
2021-05-24 19:24:26 +0100

    Styling and functionality for home and welcome/log in pages

--- front_end/static/js/body-scripts.js
+++ front_end/static/js/body-scripts.js
@@ -1,2 +1,14 @@
 // Get and Display Username
 window.onload = document.querySelectorAll('#username').forEach(x => x.textContent = get_username());
+
+// Use Nantite as Given User
+function username_given() {
+username = document.querySelector('#username-in').value;
+set_username(username) // set as current username
+eel.new_author(username)(); // create author (or carry on if author exists)
+window.location.href="http://localhost:8000/templates/projects.html";
+}
+
+function set_username(username) {
+    document.cookie = 'username=' + encodeURIComponent(username) + '; expires=Thu, 18 Dec 2100 12:00:00 UTC';
+}
