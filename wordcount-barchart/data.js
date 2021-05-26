c82687a8b8c647661a52307357218b5a56f4d68e
Eric <eoulster@gmail.com>
2021-05-26 08:47:49 -0400

    Added separate data file

---
+++ wordcount-barchart/data.js
@@ -0,0 +1,13 @@
+var data = [
+    {Wdate: "2021-01-01", Wcount: 400, Wtarget:400},
+    {Wdate: "2021-01-02", Wcount: 700, Wtarget:800},
+    {Wdate: "2021-01-03", Wcount: 1000, Wtarget:1200},
+    {Wdate: "2021-01-04", Wcount: 1200, Wtarget:1600},
+    {Wdate: "2021-01-05", Wcount: 1700, Wtarget:2000},
+    {Wdate: "2021-01-06", Wcount: 1900, Wtarget:2400},
+    {Wdate: "2021-01-07", Wcount: 3000, Wtarget:2800},
+    {Wdate: "2021-01-08", Wcount: 3500, Wtarget:3200},
+    {Wdate: "2021-01-09", Wcount: 4000, Wtarget:3800},
+    {Wdate: "2021-01-10", Wcount: 4500, Wtarget:4400},
+    {Wdate: "2021-01-11", Wcount: 5000, Wtarget:5200}
+    ];
