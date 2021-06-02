6e76594c80ab78a6c30b3b6b57a2cdee5504470f
Eric <eoulster@gmail.com>
2021-06-02 00:15:39 -0400

    Altering data

--- graph-elements/wordcount-barchart/data.js
+++ graph-elements/wordcount-barchart/data.js
@@ -1,4 +1,6 @@
-var data = [
+var data = {
+    daily:
+    [
     {Wdate: "2021-01-01", Wcount: 400, Wtarget:400},
     {Wdate: "2021-01-02", Wcount: 700, Wtarget:800},
     {Wdate: "2021-01-03", Wcount: 1000, Wtarget:1200},
@@ -9,5 +11,21 @@ var data = [
     {Wdate: "2021-01-08", Wcount: 3500, Wtarget:3200},
     {Wdate: "2021-01-09", Wcount: 4000, Wtarget:3800},
     {Wdate: "2021-01-10", Wcount: 4500, Wtarget:4400},
-    {Wdate: "2021-01-11", Wcount: 5000, Wtarget:5200}
-    ];
+    {Wdate: "2021-01-11", Wcount: 5000, Wtarget:5200},
+    {Wdate: "2021-01-12", Wcount: 5000, Wtarget:5600},
+    {Wdate: "2021-01-13", Wcount: 6000, Wtarget:6000},
+    {Wdate: "2021-01-14", Wcount: 6600, Wtarget:6400},
+    {Wdate: "2021-01-15", Wcount: 6800, Wtarget:6800},
+    {Wdate: "2021-01-16", Wcount: 7000, Wtarget:7200} 
+    ],
+    weekly:
+    [
+        {Wdate: "Wk 1/52, 2021", Wcount: 3000, Wtarget: 2800},
+        {Wdate: "Wk 2/52, 2021", Wcount: 6600, Wtarget: 6400},
+        {Wdate: "Wk 3/52, 2021", Wcount: 7000, Wtarget: 9200}
+    ],
+    monthly:
+    [
+        {Wdate: "Jan, 2021", Wcount: 7000, Wtarget: 12000}
+    ]
+};
