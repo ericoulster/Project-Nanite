63eaaae08fe646d36d5b8576bde30855d0ae9670
Eric <eoulster@gmail.com>
2021-06-20 13:56:13 -0400

    conditional color formatting

--- graph-elements/week-activity-barchart/barData.js
+++ graph-elements/week-activity-barchart/barData.js
@@ -1,11 +1,11 @@
 var barData = [
 
-    {Day: "Mon", Wcount: 747},
-    {Day: "Tues", Wcount: 342},
-    {Day: "Wed", Wcount: 392},
-    {Day: "Thu", Wcount: 992},
-    {Day: "Fri", Wcount: 722},
-    {Day: "Sat", Wcount: 802},
-    {Day: "Sun", Wcount: 562}
+    {Day: "Mon", Wcount: 747, IsMax:false},
+    {Day: "Tues", Wcount: 342, IsMax:false},
+    {Day: "Wed", Wcount: 392, IsMax:false},
+    {Day: "Thu", Wcount: 992, IsMax:true},
+    {Day: "Fri", Wcount: 722, IsMax:false},
+    {Day: "Sat", Wcount: 802, IsMax:false},
+    {Day: "Sun", Wcount: 562, IsMax:false}
 
 ]
