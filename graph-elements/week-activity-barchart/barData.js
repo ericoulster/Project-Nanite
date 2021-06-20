fd412f385c866612b26d1c0f56a3c5b725d49d3e
Eric <eoulster@gmail.com>
2021-06-20 12:56:26 -0400

    Working on weekbarchart

---
+++ graph-elements/week-activity-barchart/barData.js
@@ -0,0 +1,11 @@
+var barData = [
+
+    {Day: "Monday", Wcount: 747},
+    {Day: "Tuesday", Wcount: 342},
+    {Day: "Wednesday", Wcount: 392},
+    {Day: "Thursday", Wcount: 992},
+    {Day: "Friday", Wcount: 722},
+    {Day: "Saturday", Wcount: 802},
+    {Day: "Sunday", Wcount: 562}
+
+]
