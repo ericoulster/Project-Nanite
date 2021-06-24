44ba1605dc59fd8caf4a12085eb324e6e16f9ce6
Eric <eoulster@gmail.com>
2021-06-24 18:38:58 -0400

    building statspage func

--- sqlite_funcs.py
+++ sqlite_funcs.py
@@ -506,6 +506,16 @@ class ProjectActions:
         return barData
 
 
+    def return_wordgoal_and_deadline(self):
+        conn = sqlite3.connect(sqlite3_path)
+        cur = conn.cursor()
+        cur.execute("SELECT deadline, wordcount_goal FROM projects where project_id=?", (self.project_id,))
+        row = cur.fetchone()
+        conn.close()
+        wordgoal_and_deadline = dict({'wordgoal':row[1], 'deadline':row[0]})
+        return wordgoal_and_deadline
+        
+
     def enter_wordcount(self):
         """
         Input Daily Wordcounts
