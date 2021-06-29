b0f3f7e99bc99ecf87bb925045ef92764e1afede
Mark <the.mark.fullton@gmail.com>
2021-06-29 14:05:39 -0700

    FIXED PROGRESS BARS; edited samedate fn to do so; date parameter was actually being passed in as a date

--- sqlite_funcs.py
+++ sqlite_funcs.py
@@ -69,12 +69,12 @@ def timestamp():
 def datestamp():
     return str(datetime.now().date())
 
-def samedate(date: str) -> bool():
+def samedate(date: str) -> bool(): # For some reason date is actually a date? Not sure why???
     if date == None:
         return False
     else:
-        day = date
-        today = datetime.strptime(datestamp(), '%Y-%M-%d')
+        day = str(date.date())
+        today = datestamp()
         if day == today:
             return True
         else:
