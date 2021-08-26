6bcf1978a20cb0ac5d307d1c45e51810557d6427
Mark <the.mark.fullton@gmail.com>
2021-08-26 14:54:53 -0700

    Added function to create Author user if not in database at init

---
+++ front_end/static/js/setup.js
@@ -0,0 +1,16 @@
+const prepNaniteUser = async() => {
+    let currUsername = "Author";
+
+    try {
+        let is_in_db = await eel.get_author_id(currUsername)();
+        console.log("Author already created");
+        set_username('Author');
+    } catch {
+        console.log("Creating author")
+        let creatingAuthor = await eel.new_author(currUsername)(); 
+        set_username(currUsername);
+        console.log("Successfully created!")
+    }
+
+    window.location.href="http://localhost:8000/templates/projects.html";
+}
