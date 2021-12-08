8b9561d6a0eb4ee8b61387a64c5edcc7f15c3370
Mark <the.mark.fullton@gmail.com>
2021-12-08 08:54:02 -0800

    Added error download page; opens this in default browser

--- nanite.py
+++ nanite.py
@@ -2,6 +2,9 @@ import eel
 import nanite_eel_api
 import sqlite_funcs as sf
 
+import webbrowser
+import os
+
 sqlite3_path = sf.sqlite3_path
 
 eel.init('front_end')
@@ -16,5 +19,10 @@ sf.db_init() # create database if needed
 
 import file_access
 
-# Start Eel
-eel.start('templates/home.html', size=(700, 700), jinja_templates='templates')
+try:
+    # Start Eel
+    eel.start('templates/home.html', size=(700, 700), jinja_templates='templates')
+except:
+    # Chrome not found; redirects user to a page asking them to install chrome
+    # (Opens using user's default browser instead of via eel to avoid taking up the websocket)
+    webbrowser.open_new_tab(f"file:///{os.getcwd()}/front_end/templates/error_download_chrome.html")
