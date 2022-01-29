a108e536eb711ed1e9d81760ed2f53fc2b537002
Mark <the.mark.fullton@gmail.com>
2022-01-28 17:16:00 -0800

    Added chrome_finder and fixed issue with try-except

---
+++ chrome_finder.py
@@ -0,0 +1,19 @@
+import platform
+import os
+
+
+def has_chrome():
+    expected_chrome_locations = {
+        "Darwin":"/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome",
+        "Windows":"C:\Program Files\Google\Chrome\Application\Chrome.exe",
+        "Linux":"/usr/bin/google-chrome"
+    }
+
+    try:
+        install_path = expected_chrome_locations[platform.system()]
+    except:
+        raise NotImplemented(f"Error: {platform.system()} not recognized os")
+    
+    return os.path.exists(install_path)
+
+
--- nanite.py
+++ nanite.py
@@ -1,9 +1,10 @@
 import eel
 import nanite_eel_api
 import sqlite_funcs as sf
-
-import webbrowser
+from chrome_finder import has_chrome
 import os
+import webbrowser
+
 
 sqlite3_path = sf.sqlite3_path
 
@@ -19,5 +20,12 @@ sf.db_init() # create database if needed
 
 import file_access
 
-# Start Eel
-eel.start('templates/home.html', size=(700, 700), jinja_templates='templates')
+# Checks user's os via platform and then sees if chrome is installed in the default location
+# Will probably flag if the user installed in a wonked out location but...
+if has_chrome():
+    # Start Eel
+    eel.start('templates/home.html', size=(700, 700), jinja_templates='templates')
+else:
+    # Chrome not found; redirects user to a page asking them to install chrome
+    # (Opens using user's default browser instead of via eel to avoid taking up the websocket)
+    webbrowser.open_new_tab(f"file:///{os.getcwd()}/front_end/templates/error_download_chrome.html")
